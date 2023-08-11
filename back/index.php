feef6f222d1da97a20de9a442c69b1589739ffbe
Киану Сорокопуд <keanuben1@gmail.com>
2023-08-11 19:07:19 +0300

    clear

--- back/index.php
+++ back/index.php
@@ -1,2 +1,13 @@
 <?php
-echo "Hello world!";
+$host        = "host = 127.0.0.1";
+$port        = "port = 5432";
+$dbname      = "dbname = my_db";
+$credentials = "user = root password=root";
+
+$db = pg_connect( "$host $port $dbname $credentials"  );
+if(!$db) {
+    echo "Error : Unable to open database\n";
+} else {
+    echo "Opened database successfully\n";
+}
+?>
--- docker/php/php.dockerfile
+++ docker/php/php.dockerfile
@@ -1,1 +1,6 @@
 FROM php:8-fpm
+
+RUN apt-get update && apt-get install -y libpq-dev && docker-php-ext-install pdo pdo_pgsql && docker-php-ext-install pgsql
+
+EXPOSE 9000
+CMD ["php-fpm"]
